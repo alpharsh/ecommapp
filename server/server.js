2a7afbe2d021704c819f5025c373bc476453406b
Harsh Tripathi <alpharsh1@gmail.com>
2024-09-16 21:34:08 +0530

    basic client and server config done

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://alpharsh:4Ajp0f2DV3tu7iN0@cluster0.w6nqy.mongodb.net/"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
