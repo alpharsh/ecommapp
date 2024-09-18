56dfb97e18be9ea7ea01b17076efabd208100b7f
Harsh Tripathi <alpharsh1@gmail.com>
2024-09-18 22:04:06 +0530

    Registration MongoDB connected

--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,39 +1,64 @@
-import CommonForm from '@/components/common/form'
-import { registerFormControls } from '@/config'
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config";
+import { useToast } from "@/hooks/use-toast";
+import { registerUser } from "@/store/authSlice";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 const initialState = {
-  userName: '',
-  email: '',
-  password: '',
-}
+  userName: "",
+  email: "",
+  password: "",
+};
 
 function AuthRegister() {
+  const [formData, setFormData] = useState(initialState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const {toast} = useToast()
 
-  const [formData, setFormData] = useState(initialState)
-
-  function onSubmit() {
-    console.log(formData)
+  function onSubmit(event) {
+    event.preventDefault();
+    dispatch(registerUser(formData)).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+          message: "You can now login to your account",
+          type: "success",
+        })
+        navigate("/auth/login")
+      };
+    });
   }
 
+  console.log(formData);
+
   return (
-    <div className='mx-auto w-full max-w-md space-y-6'>
-      <div className='text-center'>
-        <h1 className='text-3xl font-bold tracking-tight text-foreground'>Create New Account</h1>
-        <p className='mt-2'>Already have an Account?
-          <Link className='font-medium ml-2 text-primary hover:underline' to='/auth/login'>Login</Link>
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create New Account
+        </h1>
+        <p className="mt-2">
+          Already have an Account?
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/login"
+          >
+            Login
+          </Link>
         </p>
       </div>
-      <CommonForm 
+      <CommonForm
         formControls={registerFormControls}
-        buttonText={'Sign Up'}
+        buttonText={"Sign Up"}
         formData={formData}
         setFormData={setFormData}
         onSubmit={onSubmit}
       />
     </div>
-  )
+  );
 }
 
-export default AuthRegister
+export default AuthRegister;
--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
