84f330af385b119fb1f68cf3a94acdce6c0fe14e
Harsh Tripathi <alpharsh1@gmail.com>
2024-10-04 22:50:10 +0530

    Add shopping cart functionality with routes and controller

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
 
 // Connect to MongoDB
 mongoose
@@ -37,5 +38,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
