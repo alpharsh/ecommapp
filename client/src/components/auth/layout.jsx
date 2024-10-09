4de3feaec6b80bc3f424237797d1415607b6c667
Harsh Tripathi <alpharsh1@gmail.com>
2024-10-09 21:47:01 +0530

    laytout UI changes

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -5,9 +5,11 @@ function AuthLayout() {
     <div className="flex min-h-screen w-full">
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
         <div className="max-w-md space-y-6 text-center text-primary-foreground">
-          <h1 className="text-4xl font-extrabold tracking-tight">
-            Welcome to ECommerce Shopping
+          <h1 className="text-6xl font-bold">
+            Welcome Back!
+            <h1 className="text-3xl font-bold mt-1 text-slate-400">to ECommerce Shopping</h1>
           </h1>
+          
         </div>
       </div>
       <div className="flex flex-1 items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
